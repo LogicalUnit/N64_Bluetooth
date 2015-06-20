7c02d7d6efafdb65edc9dd8d9dff6d4df6909338
John Crain Welsby <john.welsby@gmail.com>
2015-06-20 15:10:04 +1000

    Tidy up .ino file

--- N64_bluetooth.ino
+++ N64_bluetooth.ino
@@ -1,54 +1,35 @@
-//#include "N64_controller.h"
 #include "N64_bluetooth_HID.h"
 
-N64_controller controller;
-N64_status status, oldStatus;
+//N64_controller controller;
+//N64_status status, oldStatus;
 N64_bluetooth_HID hid;
 
 void setup()
 {
- 
-   //delay(5000);
-  
    Serial.begin(115200);  
    hid.setup();
  
- /*
-   
-   
-   */
 } //setup
 
 void loop()
 { 
- // Serial.println("loop");
-
-
   noInterrupts();  
   hid.loop();
-//  byte sendBuf[] = { 0x01 };
+  //byte sendBuf[] = { COMMAND_STATUS };
   //controller.N64_send(sendBuf, sizeof sendBuf); 
-//  controller.sendStatusByte();
-  //N64_status status; 
+  //controller.sendStatusByte();
   //controller.N64_get((byte*) &status, sizeof status);
- // status = controller.getStatus();
+  //status = controller.getStatus();
   interrupts();
   
-  delay(100);
-  
-  
-//  static N64_status oldStatus;
  /*
   if (memcmp (&status, &oldStatus, sizeof status) != 0)
   { 
     printN64status(status);
     oldStatus = status;
   }
-    
-    delay(200);
-   
    */
-  
- // delay(100);  
+   
+   delay(100);  
  
 } //loop
