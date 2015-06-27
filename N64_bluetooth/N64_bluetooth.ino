77711bfd40b9ef4aae9f45051b65c0054f039def
John Crain Welsby <john.welsby@gmail.com>
2015-06-27 18:53:51 +1000

    Only send Bluetooth updates when state has changed

--- N64_bluetooth/N64_bluetooth.ino
+++ N64_bluetooth/N64_bluetooth.ino
@@ -20,19 +20,16 @@ void loop()
   controller.sendStatusByte();
   controller.N64_get((byte*) &status, sizeof status);
   interrupts();
-  
-  if(status.stick_y == -128)
+
+  if (status.stick_y == -128) //we need to invert the y-axis, but due to the way integers are stored, -128 is like 0 and cannot change its sign
     status.stick_y = -127;
 
-  bluetooth.sendUpdate(status.stick_x, -status.stick_y, 0, 0, status.data1, status.data2);
-
-  /*
-   if (memcmp (&status, &oldStatus, sizeof status) != 0)
-   {
-     printN64status(status);
-     oldStatus = status;
-   }
-    */
+  if (memcmp (&status, &oldStatus, sizeof status) != 0) //If the status has changed
+  {
+    bluetooth.sendUpdate(status.stick_x, -status.stick_y, 0, 0, status.data1, status.data2);
+    //printN64status(status);
+    oldStatus = status;
+  }
 
   delay(100);
 
